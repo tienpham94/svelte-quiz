63281c08d2c0ee2b4f9d3972add56900ba13761c
Tien Pham <tiendev94@gmail.com>
2019-11-24 17:29:15 +0200

    shuffle

--- src/Question.svelte
+++ src/Question.svelte
@@ -1,12 +1,31 @@
 <script>
   export let question;
-  let answers = question.incorrect_answers;
+  let answers = question.incorrect_answers.map(answer => ({
+    answer,
+    correct: false
+  }));
+
+  let allAnswers = [
+    ...answers,
+    {
+      answer: question.correct_answer,
+      correct: true
+    }
+  ];
+
+  shuffle(allAnswers);
+
+  function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
 </script>
 
 <h3>
   {@html question.question}
 </h3>
 
-{#each answers as answer}
-  <button>{@html answer}</button>
+{#each allAnswers as answer}
+  <button>
+    {@html answer.answer}
+  </button>
 {/each}
