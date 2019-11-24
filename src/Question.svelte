fa3ac89e93c0a5712df1721568e4435d367de6d3
Tien Pham <tiendev94@gmail.com>
2019-11-24 17:35:27 +0200

    quiz

--- src/Question.svelte
+++ src/Question.svelte
@@ -1,5 +1,8 @@
 <script>
   export let question;
+
+  let isCorrect;
+  let isAnswered = false;
   let answers = question.incorrect_answers.map(answer => ({
     answer,
     correct: false
@@ -18,14 +21,26 @@
   function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
   }
+
+  function checkQuestion(correct) {
+    isAnswered = true;
+    isCorrect = correct;
+  }
 </script>
 
 <h3>
   {@html question.question}
 </h3>
 
+{#if isAnswered}
+  <h4>
+    {#if isCorrect}You got it right{:else}You goofed up{/if}
+
+  </h4>
+{/if}
+
 {#each allAnswers as answer}
-  <button>
+  <button on:click={() => checkQuestion(answer.correct)}>
     {@html answer.answer}
   </button>
 {/each}
