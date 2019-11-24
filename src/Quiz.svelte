081ca64afe101cc329a5f55008ebadb47971ccc9
Tien Pham <tiendev94@gmail.com>
2019-11-24 16:14:14 +0200

    more bind

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,11 +1,13 @@
 <script>
   export let quizName = "Tien Quiz";
 
-  let title = "";
+  let a = 0;
+  let b = 0;
 </script>
 
 <div>
   <h2>{quizName}</h2>
-  <h4>{title}</h4>
-  <input bind:value={title} type="text" />
+  <input bind:value={a} type="number" />
+  <input bind:value={b} type="number" />
+  <h4>{a + b}</h4>
 </div>
