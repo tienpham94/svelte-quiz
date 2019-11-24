a03c9244675d15122ad24171157ea53829d79fed
Tien Pham <tiendev94@gmail.com>
2019-11-24 16:19:02 +0200

    pick answer

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,12 +1,19 @@
 <script>
-  function pickAnswer() {
-    console.log("answered");
+  let result = "";
+  let correctAnswer = "b";
+
+  function pickAnswer(answer) {
+    if (answer === correctAnswer) {
+      return (result = "Correct!");
+    }
+    result = "Oops";
   }
 </script>
 
 <div>
-  <button on:click={pickAnswer}>Answer A</button>
-  <button on:click={pickAnswer}>Answer B</button>
-  <button on:click={pickAnswer}>Answer C</button>
-  <button on:click={pickAnswer}>Answer D</button>
+  <h4>{result}</h4>
+  <button on:click={() => pickAnswer('a')}>Answer A</button>
+  <button on:click={() => pickAnswer('b')}>Answer B</button>
+  <button on:click={() => pickAnswer('c')}>Answer C</button>
+  <button on:click={() => pickAnswer('d')}>Answer D</button>
 </div>
